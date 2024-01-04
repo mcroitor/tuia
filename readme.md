6ddd55188036d2f8f696a2923208538941355ce2
Mihail Croitor <mcroitor@gmail.com>
2024-01-04 16:00:03 +0200

    some docs

--- readme.md
+++ readme.md
@@ -21,4 +21,43 @@ Build library:
 make
 ```
 
+## Interface
+
+```cpp
+class TUIA
+{
+public:
+    static std::string ColorCode(ForegroundColor fgColor, BackgroundColor bgColor);
+    static void Init();
+    static void SetForegroundColor(const Color &foregroundColor);
+    static void SetBackgroundColor(const Color &backgroundColor);
+    static void SetForegroundColor(const ForegroundColor &foregroundColor);
+    static void SetBackgroundColor(const BackgroundColor &backgroundColor);
+    static Color GetForegroundColor();
+    static Color GetBackgroundColor();
+    static void WriteLine(const Point &position, const std::string &line);
+    static void ClearLine(const Point &position, uint32_t nChars);
+    static void ClearBlock(const Point &leftTop, const Point &rightBottom);
+    static void ClearBlock(const Point &position, uint32_t nChars, uint32_t nLines);
+    static void ClearScreen();
+    static void Draw(const Point &position, const Image &image);
+    static void SetCursor(const Point &position);
+    static Point GetScreenSize();
+    static void PutPoint(const Point& position, const Color& color);
+    static void PutPoint(const Point& position, const BackgroundColor& color);
+};
+```
+
 ## Usage
+
+```cpp
+
+using usm::graphics::TUIA;
+using usm::graphics::Point;
+using usm::graphics::Color;
+
+TUIA::Init();
+TUIA::ClearScreen();
+TUIA::ClearBlock({2, 2}, {20, 5});
+TUIA::WriteLine({2, 3}, "This is a test");
+```
--- source/tuia.hpp
+++ source/tuia.hpp
@@ -20,24 +20,104 @@ namespace usm::graphics
         static std::string ColorCode();
 
     public:
+        /**
+         * @brief Create a string representation (ASCII code) for specified colors.
+         * @param ForegroundColor
+         * @param BackgroundColor
+         * @return std::string
+         */
         static std::string ColorCode(ForegroundColor fgColor, BackgroundColor bgColor);
+        /**
+         * @brief Set initial values for background and foreground colors.
+         */
         static void Init();
+        /**
+         * @brief Set foreground color for text.
+         * @param Color will be converted to the appropiate ForegroundColor
+         */
         static void SetForegroundColor(const Color &foregroundColor);
+        /**
+         * @brief Set background color.
+         * @param Color will be converted to the appropiate BackgroundColor
+         */
         static void SetBackgroundColor(const Color &backgroundColor);
+        /**
+         * @brief Set foreground color for text.
+         * @param ForegroundColor
+         */
         static void SetForegroundColor(const ForegroundColor &foregroundColor);
+        /**
+         * @brief Set background color.
+         * @param BackgroundColor
+         */
         static void SetBackgroundColor(const BackgroundColor &backgroundColor);
+        /**
+         * @brief Get current Foreground Color
+         * @return Color
+         */
         static Color GetForegroundColor();
+        /**
+         * @brief Get current Background Color
+         * @return Color
+         */        
         static Color GetBackgroundColor();
+        /**
+         * @brief Write a text from specific point of screen.
+         * @param position point of screen
+         * @param line text for printing
+         */
         static void WriteLine(const Point &position, const std::string &line);
+        /**
+         * @brief Clear line from specific point of screen.
+         * @param position point of screen
+         * @param nChars number of symbols for clear
+         */
         static void ClearLine(const Point &position, uint32_t nChars);
+        /**
+         * @brief Clear block of screen.
+         * @param leftTop left top point of screen
+         * @param rightBottom right bottom point of screen
+         */
         static void ClearBlock(const Point &leftTop, const Point &rightBottom);
+        /**
+         * @brief Clear block of screen.
+         * @param position left top point of screen
+         * @param nChars number of symbols
+         * @param nLines number of lines
+         */
         static void ClearBlock(const Point &position, uint32_t nChars, uint32_t nLines);
+        /**
+         * @brief Clear screen.
+         */
         static void ClearScreen();
+        /**
+         * @brief Draw image from specified position.
+         * @param position left top point of screen
+         * @param image
+         */
         static void Draw(const Point &position, const Image &image);
+        /**
+         * @brief Set cursor to the specified point.
+         * @param position point of screen
+         */
         static void SetCursor(const Point &position);
+        /**
+         * @brief Return Screen size as point. X value == width, Y value == height
+         * @return Point
+         */
         static Point GetScreenSize();
 
+        /**
+         * @brief Set a color point on the screen.
+         * @param position point on the screen
+         * @param color of pixel
+         */
         static void PutPoint(const Point& position, const Color& color);
+        /**
+         * @brief Set a color point on the screen.
+         * @param position point on the screen
+         * @param color color of the pixel in BackgroundColor format
+         */
         static void PutPoint(const Point& position, const BackgroundColor& color);
     };
 }
